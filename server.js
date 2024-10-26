f90609fa2e6bd6f1df06e681878046ec4ed66fbc
GKH-07 <girishhans308@gmail.com>
2024-10-26 16:46:35 +0530

    Cors - frontend url

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const eventRoute = require('./routes/event');
 
 dotenv.config();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+}));
 
 app.use('/api/auth', authRoute);
 app.use('/api/events', eventRoute);
